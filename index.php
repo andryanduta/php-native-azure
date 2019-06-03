300e0f7862bc4977ba14af3ba371bc543154cff3
Andryan Duta <andryangama@gmail.com>
2019-06-03 08:26:29 +0700

    back

--- index.php
+++ index.php
@@ -68,9 +68,9 @@
 				    $db = "dicodingdb";
 
 				    try {
-				        $conn = new PDO("dblib:host = $host; dbname = $db", $user, $pass);
+				        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
 				        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-				    } catch(PDOException  $e) {
+				    } catch(Exception $e) {
 				        echo "Failed: " . $e;
 				    }
 
